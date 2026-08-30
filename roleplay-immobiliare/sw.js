c88ec87e0cc7b300d3d1998517f5a2b526cd9a19
josephsocialmedia2-spec <joseph.socialmedia2@gmail.com>
2026-08-30 16:02:37 +0200

    Refresh Role Play cache for acting update

--- roleplay-immobiliare/sw.js
+++ roleplay-immobiliare/sw.js
@@ -1,1 +1,11 @@
-const C='roleplay-v3';const F=['./','./index.html','./manifest.webmanifest','./pack1.js','./pack2.js','./pack3.js','./pack4.js'];self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(F))));self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==C).map(x=>caches.delete(x))))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const C='roleplay-v4';
+const F=['./','./index.html','./manifest.webmanifest','./pack1.js','./pack2.js','./pack3.js','./pack4.js'];
+self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(C).then(c=>c.addAll(F)))});
+self.addEventListener('activate',e=>e.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==C).map(k=>caches.delete(k))))])));
+self.addEventListener('fetch',e=>{
+  if(e.request.mode==='navigate'){
+    e.respondWith(fetch(e.request).then(r=>{let copy=r.clone();caches.open(C).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request).then(r=>r||caches.match('./index.html'))));
+    return;
+  }
+  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(resp=>{let copy=resp.clone();caches.open(C).then(c=>c.put(e.request,copy));return resp})));
+});
