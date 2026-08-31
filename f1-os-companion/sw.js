74da41a9fa1907f65a82ba74b107f945b25fa618
josephsocialmedia2-spec <joseph.socialmedia2@gmail.com>
2026-08-31 06:42:40 +0200

    Add F1 OS Companion service worker

---
+++ f1-os-companion/sw.js
@@ -0,0 +1,1 @@
+const C='f1-companion-v1';self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(['./','index.html','app.js','manifest.webmanifest']))));self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).catch(()=>caches.match(e.request))));
