fae968773f2c7399c8e2bd2c8bf8f476576b5f56
josephsocialmedia2-spec <joseph.socialmedia2@gmail.com>
2026-09-06 14:18:33 +0200

    Point F1 desktop launcher to port 8877

--- publisher/f1_graphics_automation/APRI_RACCOLTA_GRAFICHE.bat
+++ publisher/f1_graphics_automation/APRI_RACCOLTA_GRAFICHE.bat
@@ -2,4 +2,9 @@
 setlocal
 cd /d "%~dp0\..\.."
 powershell.exe -NoProfile -WindowStyle Hidden -ExecutionPolicy Bypass -File "%~dp0START_INBOX.ps1"
-start "F1 Raccolta Grafiche" http://127.0.0.1:8765/
+if errorlevel 1 (
+  echo F1 Raccolta Grafiche non disponibile.
+  pause
+  exit /b 1
+)
+start "F1 Raccolta Grafiche" http://127.0.0.1:8877/
