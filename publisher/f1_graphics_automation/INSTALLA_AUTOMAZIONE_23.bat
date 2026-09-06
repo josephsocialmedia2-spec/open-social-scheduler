ca98a293c18b5280e44fa2f2a8b074a50ee32bc5
josephsocialmedia2-spec <joseph.socialmedia2@gmail.com>
2026-09-06 12:56:49 +0200

    Auto-elevate F1 graphics Windows installer

--- publisher/f1_graphics_automation/INSTALLA_AUTOMAZIONE_23.bat
+++ publisher/f1_graphics_automation/INSTALLA_AUTOMAZIONE_23.bat
@@ -1,4 +1,9 @@
 @echo off
 setlocal
 cd /d "%~dp0"
+net session >nul 2>&1
+if %errorlevel% neq 0 (
+  powershell.exe -NoProfile -Command "Start-Process -FilePath '%~f0' -Verb RunAs"
+  exit /b
+)
 powershell.exe -NoProfile -ExecutionPolicy Bypass -File "%~dp0INSTALLA_AUTOMAZIONE_23.ps1"
