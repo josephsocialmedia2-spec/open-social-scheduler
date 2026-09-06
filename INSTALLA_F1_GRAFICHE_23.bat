bf7bc30ef3d361eedd8c632f11f56711474b1805
josephsocialmedia2-spec <joseph.socialmedia2@gmail.com>
2026-09-06 13:25:25 +0200

    Stop F1 installer on setup failure

--- INSTALLA_F1_GRAFICHE_23.bat
+++ INSTALLA_F1_GRAFICHE_23.bat
@@ -2,3 +2,9 @@
 setlocal
 cd /d "%~dp0"
 call publisher\f1_graphics_automation\INSTALLA_AUTOMAZIONE_23.bat
+if errorlevel 1 (
+  echo.
+  echo INSTALLAZIONE F1 NON COMPLETATA.
+  exit /b 1
+)
+exit /b 0
