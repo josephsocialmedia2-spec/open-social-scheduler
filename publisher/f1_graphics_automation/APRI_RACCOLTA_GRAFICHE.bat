42eb81dfb2c1b69b4e1e7b14521b1bd3a6704d9a
josephsocialmedia2-spec <joseph.socialmedia2@gmail.com>
2026-09-20 14:48:52 +0200

    Rebrand local launcher as F1 Autopublisher

--- publisher/f1_graphics_automation/APRI_RACCOLTA_GRAFICHE.bat
+++ publisher/f1_graphics_automation/APRI_RACCOLTA_GRAFICHE.bat
@@ -3,8 +3,8 @@ setlocal
 cd /d "%~dp0\..\.."
 powershell.exe -NoProfile -WindowStyle Hidden -ExecutionPolicy Bypass -File "%~dp0START_INBOX.ps1"
 if errorlevel 1 (
-  echo F1 Raccolta Grafiche non disponibile.
+  echo F1 Autopublisher non disponibile.
   pause
   exit /b 1
 )
-start "F1 Raccolta Grafiche" http://127.0.0.1:8877/
+start "F1 AUTOPUBLISHER" http://127.0.0.1:8877/
