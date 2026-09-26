3a2394c28c28d6365675c66424e1c932f39ea41b
open-social-pages <open-social-pages@users.noreply.github.com>
2026-09-26 18:51:32 +0000

    Sync public Open Social Scheduler controls

--- f1-content-hub/sw.js
+++ f1-content-hub/sw.js
@@ -1,4 +1,4 @@
-const CACHE="f1-content-hub-v6";
+const CACHE="f1-content-hub-v7";
 const CORE=["./vendor/supabase.min.js","./","./index.html","./immobiliare-la-sacra.html","./manifest.webmanifest","./icon.svg"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting())));
 self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE&&k.startsWith("f1-content-hub-")).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
